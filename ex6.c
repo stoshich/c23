0124c6b6c3546b2b5fd59c86ac2ac72f8f7a4ba1
stoshich <stoshich.stefan@yandex.ru>
2018-05-05 00:46:09 +0300

    ex6

---
+++ ex6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	FILE *pf;
+	int ch, i = 0;
+	char enter;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "enter the: %s file_for_reading_name\n", argv[0]); // example_big.txt
+		exit(EXIT_FAILURE);
+	}
+
+	if((pf = fopen(argv[1], "r")) == NULL)
+	{
+		fprintf(stderr, "open error %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while(enter != 'q')
+	{
+		while(i < 20)
+		{
+			while((ch = getc(pf)) != '\n')
+			{
+				putc(ch, stdout);
+				if(ch == EOF)
+					exit(EXIT_SUCCESS);
+			}
+			++i;
+			printf("\n");
+		}
+		i = 0;
+		printf("enter the symbol: ");
+		scanf("%c", &enter);
+		printf("\n");
+	}
+
+	if((fclose(pf)) != 0)
+		fprintf(stderr, "close error %s\n", argv[1]);
+
+	return 0;
+}
